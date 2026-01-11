c28c65294d3a8895ad081776f8a76050cc27a011
Sebastian Herrera <sebashm621@gmail.com>
2026-01-11 16:44:56 -0500

    feat: add back navigation link to Header component

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,21 +1,29 @@
-import { User } from "lucide-react";
+import { ArrowLeft, User } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <header className="flex justify-between items-center p-6 border-b border-black dark:border-white bg-white dark:bg-black">
-            <h1 className="text-2xl font-bold font-inconsolata text-black dark:text-white">
-                PANEL DE CONTROL
-            </h1>
-            <div className="flex items-center gap-4">
-                <div className="flex items-center gap-2 px-4 py-2 bg-gray-100 dark:bg-gray-800 rounded-full">
-                    <User size={18} className="text-black dark:text-white" />
-                    <span className="font-bold font-inconsolata text-sm text-black dark:text-white">
-                        Santi Bedoya
-                    </span>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className="flex justify-between items-center p-6 border-b border-black dark:border-white bg-white dark:bg-black">
+      <h1 className="text-2xl font-bold font-inconsolata text-black dark:text-white">
+        PANEL DE CONTROL
+      </h1>
+      <div className="flex items-center gap-4">
+        <div>
+          <Link to="/" className="px-4 py-2">
+            <ArrowLeft size={18} className="inline-block mr-2" />
+            Regresar
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-2 px-4 py-2 bg-gray-100 dark:bg-gray-800 rounded-full">
+          <User size={18} className="text-black dark:text-white" />
+          <span className="font-bold font-inconsolata text-sm text-black dark:text-white">
+            Santi Bedoya
+          </span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
